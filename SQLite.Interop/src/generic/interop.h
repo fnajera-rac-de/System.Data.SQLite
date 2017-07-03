b1c1a6ba81e8771aa3377ffa46303664a9531881
Fernando P. Najera Cano <f.najera@rac.de>
2017-07-03 09:17:11 +0200

    Import sqlite-netFx-full-source-1.0.105.2.zip

---
+++ SQLite.Interop/src/generic/interop.h
@@ -0,0 +1,18 @@
+/*
+ * interop.h -
+ *
+ * Written by Joe Mistachkin.
+ * Released to the public domain, use at your own risk!
+ */
+
+#ifndef INTEROP_VERSION
+#define INTEROP_VERSION          "1.0.105.2"
+#endif
+
+#ifndef INTEROP_SOURCE_ID
+#define INTEROP_SOURCE_ID        "c8e059414e148b3f3135b247df0045bdec86d623"
+#endif
+
+#ifndef INTEROP_SOURCE_TIMESTAMP
+#define INTEROP_SOURCE_TIMESTAMP "2017-06-10 17:51:29 UTC"
+#endif
---
+++ System.Data.SQLite/SQLitePatchLevel.cs
@@ -0,0 +1,16 @@
+/********************************************************
+ * ADO.NET 2.0 Data Provider for SQLite Version 3.X
+ * Written by Joe Mistachkin (joe@mistachkin.com)
+ *
+ * Released to the public domain, use at your own risk!
+ ********************************************************/
+
+using System.Data.SQLite;
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceId("c8e059414e148b3f3135b247df0045bdec86d623")]
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceTimeStamp("2017-06-10 17:51:29 UTC")]
